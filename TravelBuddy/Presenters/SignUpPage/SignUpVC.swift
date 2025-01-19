6fb81be484cd2c8c0901972715a5d82aa7f4f712
KochieviAna <anna.kochievi@gmail.com>
2025-01-19 11:52:45 +0100

    EnableTapToDismissKeyboard added to signup page.

--- TravelBuddy/Presenters/SignUpPage/SignUpVC.swift
+++ TravelBuddy/Presenters/SignUpPage/SignUpVC.swift
@@ -128,6 +128,8 @@ final class SignUpVC: UIViewController {
         }
         
         setupConstraints()
+        
+        enableTapToDismissKeyboard()
     }
     
     private func setupConstraints() {
